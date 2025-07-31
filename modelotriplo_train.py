158db3d6f7e857a2e01dd94165b781f73454798f
Rodrigo <rodrigo.eu.1@hotmail.com>
2025-07-30 23:34:11 -0300

    .

--- modelotriplo_train.py
+++ modelotriplo_train.py
@@ -3,18 +3,15 @@ import shutil
 import torch
 import pytorch_lightning as pl
 import numpy as np
-import wandb
 import yaml
 import random
 from pytorch_lightning.callbacks import TQDMProgressBar, ModelCheckpoint
-from pytorch_lightning.loggers import WandbLogger
 from model import CustomModelTriple
 from kf_data import CustomImageCSVModule_kf
 from callbacks import (
     EarlyStoppingAtSpecificEpoch,
     SaveBestOrLastModelCallback,
     EarlyStopCallback
-
 )
 
 # Carregar hiperparâmetros do arquivo config.yaml
@@ -32,117 +29,98 @@ def set_random_seeds():
     torch.cuda.manual_seed_all(42)
 
 # Função principal para treinamento com validação cruzada
-def train_model(config=None):
+def train_model():
     hyperparams = load_hyperparameters('config.yaml')
     k_splits = hyperparams['K_FOLDS']
     metrics_history = {}
-    
-    run_dir = os.path.join("modelos_ensemble_kf", f"{hyperparams['NAME_DATASET']}_{hyperparams['TMODEL']}")
+
+    run_dir = os.path.join("modelos_kf", f"{hyperparams['NAME_DATASET']}_{hyperparams['TMODEL']}")
     os.makedirs(run_dir, exist_ok=True)
 
-    with wandb.init(project=hyperparams["PROJECT"], config=config):
-        print(wandb.run.name)
-        config_sweep = wandb.config
+    for fold in range(k_splits):
+        print(f"\n==================== Fold {fold+1}/{k_splits} ====================")
 
-                
-        wandb_logger = WandbLogger(project=hyperparams["PROJECT"])
+        fold_callback = ModelCheckpoint(
+            dirpath=run_dir,
+            filename=f"fold_{fold}_best_model",
+            monitor="val_loss",
+            mode="min",
+            save_top_k=1
+        )
+
+        model = CustomModelTriple(
+            name_dataset=hyperparams["NAME_DATASET"],
+            shape=hyperparams["SHAPE"],
+            epochs=hyperparams['MAX_EPOCHS'],
+            learning_rate=hyperparams['LEARNING_RATE'],
+            features_dim=hyperparams["FEATURES_DIM"],
+            drop_path_rate=hyperparams['DROP_PATH_RATE'],
+            num_classes=hyperparams['NUM_CLASSES'],
+            label_smoothing=hyperparams['LABEL_SMOOTHING'],
+            optimizer_momentum=(hyperparams['OPTIMIZER_MOMENTUM'], 0.999),
+            weight_decay=hyperparams['WEIGHT_DECAY'],
+            layer_scale=hyperparams['LAYER_SCALE']=)
         
-        for fold in range(1):
 
-            print(f"\n==================== Fold {fold+1}/{k_splits} ====================")
+        data_module = CustomImageCSVModule_kf(
+            train_dir=hyperparams['TRAIN_DIR'],
+            test_dir=hyperparams['TEST_DIR'],
+            shape=hyperparams['SHAPE'],
+            batch_size=hyperparams['BATCH_SIZE'],
+            num_workers=hyperparams['NUM_WORKERS'],
+            n_splits=k_splits,
+            fold_idx=fold
+        )
+        data_module.setup(stage='fit')
 
-            fold_callback = ModelCheckpoint(
-                dirpath=run_dir,
-                filename=f"fold_{fold}_best_model",
-                monitor="val_loss",
-                mode="min",
-                save_top_k=1
-            )
+        callbacks = [
+            TQDMProgressBar(leave=True),
+            fold_callback
+        ]
 
-        
-            model = CustomModelTriple(
-                name_dataset=hyperparams["NAME_DATASET"],
-                shape=hyperparams["SHAPE"],
-                epochs=hyperparams['MAX_EPOCHS'],
-                learning_rate=float(config_sweep.learning_rate),
-                features_dim=hyperparams["FEATURES_DIM"],
-                drop_path_rate=config_sweep.drop_path_rate,
-                num_classes=hyperparams['NUM_CLASSES'],
-                label_smoothing=config_sweep.label_smoothing,
-                optimizer_momentum=(config_sweep.optimizer_momentum, 0.999),  # AdamW usa dois betas
-                weight_decay=float(config_sweep.weight_decay),
-                layer_scale=config_sweep.layer_scale)
+        trainer = pl.Trainer(
+            log_every_n_steps=10,
+            accelerator=hyperparams['ACCELERATOR'],
+            devices=hyperparams['DEVICES'],
+            precision=hyperparams['PRECISION'],
+            max_epochs=hyperparams['MAX_EPOCHS'],
+            callbacks=callbacks
+        )
 
-            print(f"\nTreinando Fold {fold+1}/{k_splits}")
+        trainer.fit(model, data_module)
 
-            data_module = CustomImageCSVModule_kf(
-                train_dir=hyperparams['TRAIN_DIR'],
-                test_dir=hyperparams['TEST_DIR'],
-                shape=hyperparams['SHAPE'],
-                batch_size=hyperparams['BATCH_SIZE'],
-                num_workers=hyperparams['NUM_WORKERS'],
-                n_splits=k_splits,
-                fold_idx=fold
-            )
-            data_module.setup(stage='fit')
+        best_model_path = fold_callback.best_model_path
 
-            callbacks = [
-                TQDMProgressBar(leave=True),
-                fold_callback
-            ]
+        # Novo trecho: carregar epoch do melhor modelo
+        checkpoint_data = torch.load(best_model_path, map_location='cpu')
+        best_epoch = checkpoint_data['epoch']
+        print(f"Melhor modelo para fold {fold} salvo na época {best_epoch}")
 
-            trainer = pl.Trainer(
-                logger=wandb_logger,
-                log_every_n_steps=10,
-                accelerator=hyperparams['ACCELERATOR'],
-                devices=hyperparams['DEVICES'],
-                precision=hyperparams['PRECISION'],
-                max_epochs=hyperparams['MAX_EPOCHS'],
-                callbacks=callbacks
-            )
+        model = CustomEnsembleModel.load_from_checkpoint(best_model_path)
+        val_metrics = trainer.validate(model, data_module)[0]
+        test_metrics = trainer.test(model, data_module)[0]
 
-            trainer.fit(model, data_module)
-            
-            best_model_path = fold_callback.best_model_path
-            model = CustomModelTriple.load_from_checkpoint(best_model_path)
-            val_metrics = trainer.validate(model, data_module)[0]
-            test_metrics = trainer.test(model, data_module)[0]
+        for metric_name, metric_value in val_metrics.items():
+            if metric_name not in metrics_history:
+                metrics_history[metric_name] = []
+            metrics_history[metric_name].append(metric_value)
 
-            for metric_name, metric_value in val_metrics.items():
-                if metric_name not in metrics_history:
-                    metrics_history[metric_name] = []
-                metrics_history[metric_name].append(metric_value)
+        for metric_name, metric_value in test_metrics.items():
+            if metric_name not in metrics_history:
+                metrics_history[metric_name] = []
+            metrics_history[metric_name].append(metric_value)
 
-            for metric_name, metric_value in test_metrics.items():
-                if metric_name not in metrics_history:
-                    metrics_history[metric_name] = []
-                metrics_history[metric_name].append(metric_value)
+        # Limpar modelo da GPU ao final do fold
+        del model
+        torch.cuda.empty_cache()
 
-        print("\n==================== Métricas Finais ====================")
-        for metric_name, values in metrics_history.items():
-            if isinstance(values[0], (int, float, np.float32, np.float64)):
-                mean = np.mean(values)
-                std = np.std(values)
-                print(f"{metric_name}: mean = {mean:.4f}, std = {std:.4f}")
-        
-    wandb.finish()
+    print("\n==================== Métricas Finais ====================")
+    for metric_name, values in metrics_history.items():
+        if isinstance(values[0], (int, float, np.float32, np.float64)):
+            mean = np.mean(values)
+            std = np.std(values)
+            print(f"{metric_name}: mean = {mean:.4f}, std = {std:.4f}")
 
 if __name__ == "__main__":
     set_random_seeds()
-    sweep_config = {
-        'method': 'random',
-        'metric': {'name': 'val_loss', 'goal': 'minimize'},
-        'parameters': {
-            'learning_rate': {'min': 1e-5, 'max': 1e-4, 'distribution': 'log_uniform_values'},
-            'weight_decay': {'min': 1e-6, 'max': 1e-3, 'distribution': 'log_uniform_values'},
-            'optimizer_momentum': {'min': 0.92, 'max': 0.99, 'distribution': 'uniform'},
-            'mlp_vector_model_scale': {'min': 0.8, 'max': 1.3, 'distribution': 'uniform'},
-            'layer_scale': {'min': 1, 'max': 5, 'distribution': 'uniform'},
-            'drop_path_rate': {'min': 0.0, 'max': 0.5, 'distribution': 'uniform'},
-            'label_smoothing': {'min': 0.0, 'max': 0.2, 'distribution': 'uniform'}
-        }
-    }
-
-    sweep_id = wandb.sweep(sweep_config, project=load_hyperparameters('config.yaml')["PROJECT"])
-    wandb.agent(sweep_id, function=train_model, count=200)
-    wandb.finish()
+    train_model()
