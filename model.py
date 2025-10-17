a75feb27b7b5206a332bbc535e0159ec2fb4cd7a
Rodrigo <rodrigo.eu.1@hotmail.com>
2025-10-16 21:06:32 -0300

    .

--- model.py
+++ model.py
@@ -1322,7 +1322,7 @@ class CustomFeaturesOnlyModelDropIn(pl.LightningModule):
         self.save_hyperparameters()
 
         self.num_classes = int(num_classes)
-        self.features_dim = int(features_dim)
+        self.features_dim = 1296
         self.learning_rate = float(learning_rate)
         self.weight_decay = float(weight_decay)
         self.optimizer_momentum = tuple(optimizer_momentum) if isinstance(optimizer_momentum, (list, tuple)) else (0.9, 0.999)
